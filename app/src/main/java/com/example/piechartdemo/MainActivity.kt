236f61b6a26fa0117967afa819241e75619cd284
rohit <rohit.ramtrith@thinkitive.com>
2019-06-19 20:37:28 +0530

    initial commit

---
+++ app/src/main/java/com/example/piechartdemo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.piechartdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
